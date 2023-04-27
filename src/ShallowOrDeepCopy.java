118661b947ae2e674c27cc948943c6de5938d19d
Bruno <brunoconcli@gmail.com>
2023-04-27 05:34:40 -0300

    feat: Add ShallowOrDeepCopy class

---
+++ src/ShallowOrDeepCopy.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Method;
+public class ShallowOrDeepCopy {
+    public static Object verifyAndCopy(Object data) {
+        if(data instanceof Cloneable) return deepCopy(data);
+        else return data;
+    }
+
+    public static Object deepCopy(Object data) {
+        try {
+            Class<?> x = data.getClass();
+            Method method = x.getMethod("clone");
+            return (Object)method.invoke(data);
+        } catch(Exception err) { err.printStackTrace(); return null; }
+    }
+}
